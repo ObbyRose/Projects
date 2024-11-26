d4b46393ec9d37c6735def11243ea451de63fa54
Tal Maman <dreapet41s@gmail.com>
2024-11-26 12:25:40 +0200

    .

--- BackendProject/mern-auth-frontend/src/App.jsx
+++ BackendProject/mern-auth-frontend/src/App.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import SignUp from '../components/SignUp/SignUp.jsx';
 import SignIn from '../components/SignIn/SignIn.jsx';
+import Home from '../pages/Home.jsx'
 
 const App = () => {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Navigate to="/signin" replace />} />
         <Route path="/signin" element={<SignIn />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/signup" element={<SignUp />} />
       </Routes>
     </Router>
