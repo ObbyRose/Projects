bd6a3f244ffb9c6b0de2e489cd72f9f2d85817a1
Tal Maman <dreapet41s@gmail.com>
2024-12-25 13:35:06 +0200

    add utility functions and components: implement class name merging utility, create skeleton, collapsible, input, tooltip, and avatar components; update TypeScript configuration for path aliases

--- business-finder/frontend/src/App.tsx
+++ business-finder/frontend/src/App.tsx
@@ -1,11 +1,16 @@
-import './App.css'
+import { AppSidebar } from './components/app-sidebar'
+import { SidebarProvider, SidebarTrigger } from './components/ui/sidebar'
 
 function App() {
 
 
   return (
     <>
-      
+        <SidebarProvider defaultOpen={false}>
+        <AppSidebar/>
+        <SidebarTrigger></SidebarTrigger>
+        </SidebarProvider>
+        <h1>HAHA</h1>
     </>
   )
 }
--- business-finder/frontend/src/main.tsx
+++ business-finder/frontend/src/main.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './index.css';
-import App from './App';
+import App from './App.tsx';
 
 const queryClient = new QueryClient();
 
