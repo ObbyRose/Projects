6572b41389da5bb07ca5b020c632a15bd240776a
Tal Maman <dreapet41s@gmail.com>
2024-12-30 12:54:06 +0200

    fix: update static file serving for production environment in server.js

--- threads-clone/backend/server.js
+++ threads-clone/backend/server.js
@@ -26,6 +26,7 @@ cloudinary.config({
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
 // Routes
 app.use("/api/users", userRoutes);
@@ -34,7 +35,6 @@ app.use("/api/messages", messageRoutes);
 
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 	app.get("*", (req, res) => {
 		res.sendFile(path.resolve(__dirname, "../../frontend/dist", "index.html"));
 	});
